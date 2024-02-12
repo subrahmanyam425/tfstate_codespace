20552f94a93fb752cdfc2d87fb92bc3ff21d3aad
subrahmanyam425 <81842364+subrahmanyam425@users.noreply.github.com>
2024-02-12 11:00:04 +0000

    demo3

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
 resource "aws_instance" "ec2" {
   instance_type = "t2.micro"
   ami = "ami-03f4878755434977f" 
-  subnet_id = "subnet-0e5263530744d3219"
+  subnet_id = aws_subnet.my_subnet.id
   tags = {
     Name = "Session_2_Demo"
+  }
+}
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "Session2_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  #availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Session2_subnet"
   }
 }
