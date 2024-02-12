55d875cf09d0bcd0dfd167f29178529cc68a4381
subrahmanyam425 <81842364+subrahmanyam425@users.noreply.github.com>
2024-02-12 10:21:49 +0000

    demo

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "subbu-s3-demo-xyz" # change this
-    key            = "subbu/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
